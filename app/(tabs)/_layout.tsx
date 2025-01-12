99715ea9d9ff09897e9be2ed3e7ccc8a4f830a52
Bufib <bufib.bufib@gmail.com>
2025-01-12 20:28:15 +0100

    before

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,12 +1,10 @@
 import { Tabs } from "expo-router";
 import React from "react";
-import { router, useSegments } from "expo-router";
 import { HapticTab } from "@/components/HapticTab";
 import { IconSymbol } from "@/components/ui/IconSymbol";
 import TabBarBackground from "@/components/ui/TabBarBackground";
 import { Colors } from "@/constants/Colors";
 import { useColorScheme } from "@/hooks/useColorScheme";
-import { string } from "zod";
 
 export default function TabLayout() {
   const colorScheme = useColorScheme();
